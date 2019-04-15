b44c09791c58a83dafe5b117c57bcb4170498d59
Nikolett Vastag <Nikolett_Vastag@epam.com>
2019-04-15 15:41:25 +0200

    initial commit

---
+++ src/module02/task01/Mentoring.System.Extensions.Tests/RomanNumeralsTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mentoring.System.Extensions.Tests
+{
+    [TestClass]
+    public class RomanNumeralsTests
+    {
+        [TestMethod]
+        public void Returns_Correct_Value_For_One()
+        {
+            // Given
+            var testValues = new Dictionary<int, string>
+            {
+                [1] = "I",
+                [2] = "II",
+                [3] = "III",
+                [4] = "IV",
+                [7] = "VII",
+                [324] = "CCCXXIV",
+                [1987] = "MCMLXXXVII",
+                [3999] = "MMMCMXCIX",
+            };
+
+            foreach (var testValue in testValues)
+            {
+                var romanNumber = testValue.Key.ToRoman();
+                Assert.AreEqual(testValue.Value, romanNumber);
+            }
+        }
+
+        [TestMethod]
+        public void Throws_Exception_For_Zero()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => 0.ToRoman());
+        }
+
+        [TestMethod]
+        public void Throws_Exception_For_Negative_Numbers()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => (-450).ToRoman());
+        }
+
+        [TestMethod]
+        public void Throws_Exception_If_Number_Larger_Than_3999()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => 4000.ToRoman());
+
+        }
+    }
+}
---
+++ src/module02/task01/Mentoring.System.Extensions/RomanNumerals.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mentoring.System.Extensions
+{
+    public static class IntExtensions
+    {
+        private static readonly IReadOnlyDictionary<int, string> ArabicToRomanMapping = new Dictionary<int, string>
+        {
+            {1, "I"},
+            {4, "IV"},
+            {5, "V"},
+            {9, "IX"},
+            {10, "X"},
+            {40, "XL"},
+            {50, "L"},
+            {90, "XC"},
+            {100, "C"},
+            {400, "CD"},
+            {500, "D"},
+            {900, "CM"},
+            {1000, "M"},
+        };
+
+        private static int GetClosest(int number) => ArabicToRomanMapping.Keys.Where(i => i <= number).Max();
+
+
+        public static string ToRoman(this int number)
+        {
+            if (number <= 0 || number >= 4000)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number), number,
+                    "The number must be between 1 and 3999.");
+            }
+
+            var closestToInput = GetClosest(number);
+
+            return number == closestToInput
+                ? ArabicToRomanMapping[number]
+                : ArabicToRomanMapping[closestToInput] + ToRoman(number - closestToInput);
+        }
+    }
+}
